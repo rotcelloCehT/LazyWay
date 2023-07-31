4775db2796296780de219d5ab3cdbb312d4216b4
Tony Radin <anthonyradin123@gmail.com>
2023-07-31 17:24:28 +0200

    working1 proper save

--- working1.js
+++ working1.js
@@ -1,18 +1,5 @@
 const puppeteer = require('puppeteer');
-
-
-// Function to write the variable to a .txt file
-function writeToFile(variableValue, filename) {
-  fs.writeFile(filename, variableValue, (err) => {
-    if (err) {
-      console.error('Error writing to file:', err);
-    } else {
-      console.log('Variable written to file successfully.');
-    }
-  });
-}
-
-
+const fs = require('fs');
 
 (async () => {
   const browser = await puppeteer.launch({
@@ -23,24 +10,31 @@ function writeToFile(variableValue, filename) {
   const page = await browser.newPage();
 
   await page.goto('https://www.pararius.com/apartments/amsterdam/sort-price-low');
-  
-  // let's just call them listingHandle 
+
   const searchList = await page.$$("ul.search-list > li.search-list__item--listing ");
-  for(const listingBox of searchList){
-    // pass the single handle below
+  const results = [];
+  for (const listingBox of searchList) {
     try {
-      const title = await page.evaluate(el => el.querySelector(".listing-search-item__title > a").textContent.trim(), listingBox)
-      console.log(title)
+      const title = await page.evaluate(el => el.querySelector(".listing-search-item__title > a").textContent.trim(), listingBox);
+      const price = await page.evaluate(el => el.querySelector(".listing-search-item__price").textContent.trim(), listingBox);
+      const link = await page.evaluate(el => el.querySelector(".listing-search-item__title > a").href, listingBox);
       
-      const price = await page.evaluate(el => el.querySelector(".listing-search-item__price").textContent.trim(), listingBox)
-      console.log(price)
+      const listingInfo = { title, price, link };
 
-      const link = await page.evaluate(el => el.querySelector(".listing-search-item__title > a").href, listingBox)
-      console.log(link)
-
+      results.push(listingInfo);
     } catch (error) {}
- }
-  // await writeToFile(results, 'output.txt');
-  await browser.close();
+  }
 
+  const resultsJson = JSON.stringify(results, null, 2);
+  console.log(resultsJson);
+
+  fs.writeFile('output.txt', resultsJson, (err) => {
+    if (err) {
+      console.error('Error writing to file:', err);
+    } else {
+      console.log('Data written to output.txt successfully.');
+    }
+  });
+
+  await browser.close();
 })();
